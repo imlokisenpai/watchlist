98b7c8514de11ec34e05ee81b6e1d1532e192ec9
Souto751 <agus.souto75@gmail.com>
2021-02-03 09:08:17 -0300

    update 3/2

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,22 +20,22 @@ export default class Header extends Component {
                         <div className="links">
                             <h1 className="webTitle">Pupi & Puxu's Watchlist</h1>
                             <div className="link-list">
-                                <Link to="/watchlist/" className="link">Home</Link>
-                                <Link to="/watchlist/animes" className="link">Animes</Link>
-                                <Link to="/watchlist/movies" className="link">Movies</Link>
-                                <Link to="/watchlist/series" className="link">Series</Link>
+                                <Link exact to="/watchlist/" className="link">Home</Link>
+                                <Link exact to="/watchlist/animes" className="link">Animes</Link>
+                                <Link exact to="/watchlist/movies" className="link">Movies</Link>
+                                <Link exact to="/watchlist/series" className="link">Series</Link>
                             </div>
                         </div>
-                        <Route exact path="/" Component={Home}>
+                        <Route exact path="/watchlist/" Component={Home}>
                             <Home />
                         </Route>
-                        <Route exact path="/animes" Component={Animes}>
+                        <Route exact path="/watchlist/animes" Component={Animes}>
                             <Animes />
                         </Route>
-                        <Route exact path="/movies" Component={Movies}>
+                        <Route exact path="/watchlist/movies" Component={Movies}>
                             <Movies />
                         </Route>
-                        <Route exact path="/series" Component={Series}>
+                        <Route exact path="/watchlist/series" Component={Series}>
                             <Series />
                         </Route>
                     </Router>
